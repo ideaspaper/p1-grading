35ea7ef1ea2eedcee3a72c6fb0502bf4ff693e7a
ideaspaper <agni.infinity@gmail.com>
2021-02-04 09:13:46 +0700

    refactor: remove -- from commands feat: add conf command

--- p1-grading.sh
+++ p1-grading.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ERR_MSG_CLONE="No config defined. Use [org] command first to set GitHub organization name. See help."
+ERR_MSG_NO_CONFIG="No config defined. Use [org] command first to set GitHub organization name. See help."
 ERR_MSG_WRONG_FORMAT="Wrong command format. See help."
 SUCCESS_MSG_ORG="Organization has been set as"
 GIT_GITHUB="git@github.com"
@@ -7,15 +7,26 @@ GIT_GITHUB="git@github.com"
 command=$1
 param=$2
 
-if [ $command = "--help" ]
+if [ $command = "help" ]
 then
     echo "Tool for cloning Hacktiv8 Phase 1 repositories."
     echo "Usages:"
-    echo "  > org <GitHub organization name> | example: org rmt-09-istanbul-fox"
-    echo "  > clone <weekday> | example: clone w1d1"
+    echo "  > org <GitHub organization name> | example: ./p1-grading.sh org rmt-09-istanbul-fox"
+    echo "  > conf                           | example: ./p1-grading.sh conf"
+    echo "  > clone <weekday>                | example: ./p1-grading.sh clone w1d1"
 fi
 
-if [ $command = "--org" ]
+if [ $command = "conf" ]
+then
+    if [ ! -s .config ]
+    then
+        echo $ERR_MSG_NO_CONFIG
+    else
+        echo `cat .config`
+    fi
+fi
+
+if [ $command = "org" ]
 then
     if [ ! -z $param ]
     then
@@ -30,13 +41,13 @@ then
     fi
 fi
 
-if [ $command = "--clone" ]
+if [ $command = "clone" ]
 then
     if [ ! -z $param ]
     then
         if [ ! -s .config ]
         then
-            echo $ERR_MSG_CLONE
+            echo $ERR_MSG_NO_CONFIG
         else
             org=`cat .config`
             rm -rf "submissions/$param-${org}"
